afd78e4eac37da1f38cf1633ff938d70c3b78c81
Shrishti Maithani <38742982+shrishtimaithani@users.noreply.github.com>
2019-07-24 15:16:17 +0530

    added the applications folder

---
+++ application/views/templates/footer.php
@@ -0,0 +1,13 @@
+
+  <footer class="main-footer">
+    <strong>UJVN Limited &copy;</strong>
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
